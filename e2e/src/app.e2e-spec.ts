63b40d08d63db93d8371962f8ae05e3cf3e83fcc
Joseph T. Ball <jball321@live.com>
2018-11-11 10:28:42 -0500

    removed previous files related to React

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to video-village!');
+    expect(page.getParagraphText()).toEqual('Welcome to Video-Village!');
   });
 });
