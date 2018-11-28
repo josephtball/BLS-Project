e435535203e3b4b8c270937218acda175d7b200a
Joseph T. Ball <jball321@live.com>
2018-11-28 12:41:57 -0500

    Connected app to The Movie Database and fetched data to home component

--- src/app/the-movie-db.service.ts
+++ src/app/the-movie-db.service.ts
@@ -5,7 +5,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class TheMovieDbService {
+  TMDb_KEY = 'api_key=6ac23abc8d5623a742214ffc539337c5';
+  TMDb_URL = 'https://api.themoviedb.org/3/';
 
-  constructor(private theMovieDb: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies() {
+    return this.http.get(this.TMDb_URL + 'movie/popular?' + this.TMDb_KEY);
+  }
 
 }
